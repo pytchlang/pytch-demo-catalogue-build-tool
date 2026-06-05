5a07d6325cc0e505368649f6ea5f351dcdecfcc6
Ben North <ben@redfrontdoor.org>
2026-06-05 16:34:10 +0100

    build-dist: Define script entry point

--- build-tool/src/pytch_demo_catalogue_build_tool/cli.py
+++ build-tool/src/pytch_demo_catalogue_build_tool/cli.py
@@ -1,7 +1,16 @@
 """Console scripts for pytchbuild"""
 
 import click
+from pathlib import Path
+import pytch_demo_catalogue_build_tool.version_data
 
 
 def dir_argument():
     return click.Path(file_okay=False, dir_okay=True)
+
+
+@click.command()
+@click.argument("demos-repo", type=dir_argument())
+@click.argument("dist-root", type=dir_argument())
+def build_dist(demos_repo: str, dist_root: str):
+    pytch_demo_catalogue_build_tool.version_data.main(Path(demos_repo), Path(dist_root))
