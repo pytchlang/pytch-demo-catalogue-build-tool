32357cb4f6eda5eba702187e3db07c8d4d66aeb6
Ben North <ben@redfrontdoor.org>
2026-06-05 16:19:26 +0100

    Define constants re MIME-type file suffix

--- build-tool/src/pytch_demo_catalogue_build_tool/constants.py
+++ build-tool/src/pytch_demo_catalogue_build_tool/constants.py
@@ -2,3 +2,15 @@ import mimetypes
 from pathlib import Path
 
 mimetypes.init()
+
+
+def _extensions_for_major_type(mime_major_type: str) -> list[str]:
+    return [
+        ext
+        for ext, mimetype in mimetypes.types_map.items()
+        if mimetype.startswith(f"{mime_major_type}/")
+    ]
+
+
+image_extensions = _extensions_for_major_type("image")
+video_extensions = _extensions_for_major_type("video")
