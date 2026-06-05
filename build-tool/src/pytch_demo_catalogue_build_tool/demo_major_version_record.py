ca947606edb380eab8c37c4b35bf8aef6e685f50
Ben North <ben.north@tcd.ie>
2026-06-05 12:03:05 +0100

    DemoMajorVersionRecord: Define simple props

--- build-tool/src/pytch_demo_catalogue_build_tool/demo_major_version_record.py
+++ build-tool/src/pytch_demo_catalogue_build_tool/demo_major_version_record.py
@@ -13,3 +13,30 @@ class DemoMajorVersionRecord(MultiLocaleDemo):
     latest_uuid: str
     _defining_commit_id: str
     _demo_root_path: Path
+
+    @property
+    def repo(self) -> pygit2.Repository:
+        return self._repo
+
+    @property
+    def defining_commit_id(self) -> str:
+        return self._defining_commit_id
+
+    @property
+    def demo_root_path(self) -> Path:
+        return self._demo_root_path
+
+    @property
+    def is_latest(self) -> bool:
+        return self.uuid == self.latest_uuid
+
+    @property
+    def commit(self) -> pygit2.Commit:
+        repo_obj = self.repo[self.defining_commit_id]
+        if not isinstance(repo_obj, pygit2.Commit):
+            raise RuntimeError(f"id {self.defining_commit_id} did not give Commit")
+        return repo_obj
+
+    @property
+    def tree(self) -> pygit2.Tree:
+        return self.commit.tree
