49572b72c9bd35a8b1d16b6e571de8d134eeee94
Ben North <ben.north@tcd.ie>
2026-06-05 11:44:48 +0100

    MultiLocaleDemo: Define ABC

---
+++ build-tool/src/pytch_demo_catalogue_build_tool/demo_locale_context.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import Any
+
+import pygit2
+
+
+class MultiLocaleDemo(ABC):
+    @property
+    @abstractmethod
+    def repo(self) -> pygit2.Repository: ...
+
+    @property
+    @abstractmethod
+    def defining_commit_id(self) -> str: ...
+
+    @property
+    @abstractmethod
+    def demo_root_path(self) -> Path: ...
+
+    @abstractmethod
+    def json_dict_within_commit(self, path: Path) -> dict[str, Any]: ...
+
+    @abstractmethod
+    def json_list_within_commit(self, path: Path) -> list[Any]: ...
+
+    @abstractmethod
+    def file_within_commit(self, path: Path) -> bytes: ...
