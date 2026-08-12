7c0b3ad9a7d00ea3dc9ad74e46f0bc3bb6e27b07
Ben North <ben@redfrontdoor.org>
2026-08-12 15:51:56 +0100

    tree_entry_within_commit(): Simplify

--- build-tool/src/pytch_demo_catalogue_build_tool/repo_files.py
+++ build-tool/src/pytch_demo_catalogue_build_tool/repo_files.py
@@ -13,34 +13,6 @@ def name_of_tree_entry(entry: pygit2.Object) -> str:
     return entry.name
 
 
-def tree_entry_within_commit(
-    repo: pygit2.Repository, commit_id: str, path: Path, exp_type: str
-) -> pygit2.Tree | pygit2.Blob:
-    if (commit := repo.get(commit_id)) is None:
-        raise KeyError(f"commit {commit_id} not found in repo")
-
-    entry = commit.tree
-    n_parts = len(path.parts)
-    for idx, path_part in enumerate(path.parts):
-        try:
-            next_entry = entry / path_part  # type: ignore
-        except KeyError:
-            raise RuntimeError(
-                f'"{path_part}" not found in "{"/".join(path.parts[:idx])}"'
-                f" within tree of commit {commit_id}"
-            )
-        is_last = idx == n_parts - 1
-        this_exp_type = exp_type if is_last else "tree"
-        if next_entry.type_str != this_exp_type:  # type: ignore
-            raise RuntimeError(
-                f"expecting {this_exp_type} at posn {idx} in path"
-                f' when processing "{path}" within tree of commit {commit_id}'
-            )
-        entry = next_entry  # type: ignore
-
-    return entry  # type: ignore
-
-
 def maybe_tree_entry_within_commit(
     repo: pygit2.Repository, commit_id: str, path: Path
 ) -> pygit2.Tree | pygit2.Blob | None:
@@ -77,6 +49,33 @@ def maybe_tree_entry_within_commit(
         entry = next_entry  # type: ignore
 
     return entry  # type: ignore
+
+
+def tree_entry_within_commit(
+    repo: pygit2.Repository, commit_id: str, path: Path, exp_type: str
+) -> pygit2.Tree | pygit2.Blob:
+    """Return entry at `path` from the tree of commit `commit_id`.
+
+    The entry named by the last component of `path` must exist and be of
+    type `exp_type`.  RuntimeError is raised if not.
+    """
+    entry = maybe_tree_entry_within_commit(repo, commit_id, path)
+
+    if entry is None:
+        raise RuntimeError(
+            f'"{path.parts[-1]}" not found in "{"/".join(path.parts[:-1])}"'
+            f" within tree of commit {commit_id}"
+        )
+
+    entry_type: str = entry.type_str
+    if entry_type != exp_type:
+        raise RuntimeError(
+            f'expecting "{exp_type}" as last component of path'
+            f' when processing "{path}" within tree of commit {commit_id}'
+            f' but found "{entry_type}"'
+        )
+
+    return entry
 
 
 def file_within_commit(repo: pygit2.Repository, commit_id: str, path: Path) -> bytes:
